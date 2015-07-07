4ad7f6a15f0404ec17280ba21ac9622a4e1ec9ad
Charles Crame <ccrame@gmail.com>
2015-07-06 17:00:51 -0700

    Completed grub.js

--- src/Bee.js
+++ src/Bee.js
@@ -1,3 +1,11 @@
 var Bee = function() {
+	Grub.call(this);
+  this.age    = 5;  
+  this.color  = 'yellow';
+  this.food   = 'jelly';
+  this.job    = 'keep on growing';
 };
 
+Bee.prototype = Object.create(Grub);
+Bee.prototype.constructor = Bee;
+Bee.prototype.eat  = function(){};
